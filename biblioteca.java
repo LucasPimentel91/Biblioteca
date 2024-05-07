8d176b7164f5a9d03619381bba90ddd79f191d15
Lucas Pimentel <placte40@gmail.com>
2024-05-07 16:21:07 -0300

    Título do Menu

--- biblioteca.java
+++ biblioteca.java
@@ -3,7 +3,7 @@ package projeto_tomorrow;
 public class biblioteca {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Biblioteco Clarice Lispector");
 
 	}
 
