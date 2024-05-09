e6bb2d06a9d3f4d3d68b37516ed93420a249793e
Lucas Pimentel <placte40@gmail.com>
2024-05-08 23:15:30 -0300

    implementando cadastroCliente e cabecalho

--- biblioteca.java
+++ biblioteca.java
@@ -1,9 +1,43 @@
 package projeto_tomorrow;
 
-public class biblioteca {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		System.out.println("Biblioteco Clarice Lispector");
+public class biblioteca {
+	
+	
+	public static void cabecalho() {
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("\tBIBLIOTECA");
+		System.out.println("--------------------------");
+		System.out.println("--------------------------");
+		System.out.println("1. Cadastrar Cliente");
+		System.out.println("2. Cadastrar Livro");
+		System.out.println("3. Cadastrar Seção");
+		System.out.println("4. Listar");
+		System.out.println("--------------------------");
+	}
+	
+	public static void cadastroCliente() {
+		Scanner texto = new Scanner(System.in);
+		cliente primeiro = new cliente();
+		System.out.println("Digite o nome: ");
+		primeiro.nome = texto.nextLine();
+		System.out.println("Digite a data de nascimento (dd/mm/aaaa): ");
+		primeiro.dataNascimento = texto.nextLine();
+		System.out.println("Digite o sexo (m- masculino/f- feminino)");
+		primeiro.sexo = texto.nextLine();
+		System.out.println("Digite o telefone de contato:");
+		System.out.println("OBS: (DDD)9xxxx-xxxx");
+		primeiro.telefone = texto.nextLine();
+		System.out.println("Digite o CPF (sem pontos e hífen): ");
+		primeiro.CPF = texto.nextLine();
+		
+		texto.close();
+	}
+
+	public static void main(String[] args) { 
+		cabecalho();
 
 	}
 
---
+++ cliente.java
@@ -0,0 +1,11 @@
+package projeto_tomorrow;
+
+public class cliente {
+	String nome;
+	String dataNascimento;
+	String sexo;
+	String telefone;
+	String CPF;
+	String cidade, estado, bairro, rua;
+	int num_logra;	
+}
